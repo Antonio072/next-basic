06918be4856dc98c9b5548b5a02d4b77945ccd4c
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2023-05-22 16:29:06 -0600

    wip: Create chart component

---
+++ src/components/Chart.js
@@ -0,0 +1,25 @@
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, BarController, Tooltip, Legend } from 'chart.js'
+import { Bar } from 'react-chartjs-2'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, BarController, Tooltip, Legend)
+
+export const Chart = ({ data }) => {
+  return (
+    <>
+      <Bar
+        data={data}
+        options={{
+          title: {
+            display: true,
+            text: 'Category',
+            fontSize: 20,
+          },
+          legend: {
+            display: true,
+            position: 'right',
+          },
+        }}
+      ></Bar>
+    </>
+  )
+}
